e9552d6dbcfc1467a90db29d1a00c5a7c5378361
IvanIG3 <ivaniglesias3gh@gmail.com>
2021-01-08 16:36:43 +0100

    added footer

---
+++ components/layout/Footer.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Github } from '@styled-icons/boxicons-logos/Github';
+import { useTranslation } from 'react-i18next';
+import Image from 'next/image';
+import Container from '../../styles/Container';
+
+const FooterContainer = styled(Container)`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-top: 1px solid;
+    padding: 15px 0;
+    margin-top: 30px;
+    position: relative;
+`;
+
+const Reference = styled.a`
+    display: flex;
+    margin: 10px 0;
+    font-size: 1rem;
+    span {
+        margin-left: 5px;
+    }
+`;
+
+const TmdbLogo = styled.a`
+    bottom: 10px;
+    right: 0px;
+    margin-top: 20px;
+    @media (min-width: 600px) {
+        position: absolute;
+    }
+`;
+
+const Footer = () => {
+    const { t } = useTranslation();
+    return (
+        <footer>
+            <FooterContainer>
+                <Reference 
+                    href="https://github.com/IvanIG3"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    {t('Designed & developed by IvanIG3')}
+                </Reference>
+                <Reference
+                    href="https://github.com/IvanIG3/super8"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Github style={{width: "1em"}}/>
+                    <span>{t('View Code')}</span>
+                </Reference>
+                <TmdbLogo 
+                    href="https://www.themoviedb.org/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image 
+                        src="/tmdb.svg" 
+                        alt="Logo TMDB"
+                        width={80}
+                        height={80}
+                    />
+                </TmdbLogo>
+            </FooterContainer>
+            
+        </footer>
+    );
+}
+ 
+export default React.memo(Footer);
