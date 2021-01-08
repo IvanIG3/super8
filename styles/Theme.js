58aeec9e263bdf6d63d1d3ca6491165efa2747c4
IvanIG3 <ivaniglesias3gh@gmail.com>
2021-01-08 11:17:36 +0100

    styled components config

---
+++ styles/Theme.js
@@ -0,0 +1,23 @@
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+    colors: {
+        secondary: '#752b00',
+        primary: '#f7a000',
+        light: '#839496',
+        dark: '#002630',
+        body: '#d0d0d0',
+    },
+    fonts: ["sans-serif", "Roboto"],
+    fontSizes: {
+        small: "1em",
+        medium: "1.2em",
+        large: "1.4em"
+    }
+};
+
+const Theme = ({ children }) => (
+    <ThemeProvider theme={theme}>{children}</ThemeProvider>
+);
+
+export default Theme;
