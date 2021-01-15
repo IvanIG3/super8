fe8c0125dad2bc62ef035266a355239600e25920
IvanIG3 <ivaniglesias3gh@gmail.com>
2021-01-15 20:10:49 +0100

    tmdb api connection

---
+++ pages/api/tmdb.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const tmdb = axios.create({
+    baseURL: process.env.tmdbURL
+});
+
+export default async function handler(req, res) {
+    // Get data from request
+    const data = req.body;
+    const endpoint = data.endpoint;
+    const params = data.params || {};
+    params['api_key'] = process.env.TMDB_API_KEY;
+    // Create api url call
+    const urlparams = new URLSearchParams(params).toString();
+    const call = `${endpoint}?${urlparams}`;
+    // Do api call and return the result data
+    try {
+        const movies = await tmdb.get(call);
+        res.status(200).json(movies.data);
+    } catch (error) {
+        res.status(500).json({ 
+            msg: "Error", 
+            error 
+        });
+    }
+};
