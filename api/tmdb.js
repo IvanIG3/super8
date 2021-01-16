517a33cca5a2465e179c24f80516920f00bfd845
IvanIG3 <ivaniglesias3gh@gmail.com>
2021-01-16 19:02:00 +0100

    tmdb api call from client instead from server

--- api/tmdb.js
+++ api/tmdb.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 const tmdb = async (endpoint, params) => {
-    const response = await axios.post('/api/tmdb', { endpoint, params });
+    params['api_key'] = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+    // Create api url call
+    const urlparams = new URLSearchParams(params).toString();
+    const call = `${process.env.tmdbURL}${endpoint}?${urlparams}`;
+    // Do api call and return the result data
+    const response = await axios.get(call);
     return response.data;
 };
 
